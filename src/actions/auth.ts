5e9ca3cd578436e59b08d2b896d49c7d7caaba38
Belal Muhammad <belallmuhammad0@gmail.com>
2026-07-07 08:48:54 +0300

    refactor: enhance auth actions

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,16 +1,21 @@
 "use server";
 
-import { setSession, clearSession } from "@/lib/session";
 import { redirect } from "next/navigation";
-import { loginSchema, type LoginFormValues } from "../dtos/auth";
+
+import { setSession, clearSession } from "@/lib/session";
+import { loginSchema, type LoginFormValues } from "@/dtos/auth";
 import { getUserByEmail } from "@/dal/users/queries";
 
 export async function login(data: LoginFormValues) {
   const result = loginSchema.safeParse(data);
-  if (!result.success) return { message: "Invalid data" };
+  if (!result.success) {
+    return { message: "Invalid data" };
+  }
 
   const user = await getUserByEmail(result.data.email);
-  if (user == null) return { message: "User not found" };
+  if (user == null) {
+    return { message: "User not found" };
+  }
 
   await setSession(user.id);
   return redirect("/projects");
