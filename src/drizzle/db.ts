9849f45d35401b650fc079ba6372927d11cade02
Belal Muhammad <belallmuhammad0@gmail.com>
2026-07-07 05:25:13 +0300

    initial commit

---
+++ src/drizzle/db.ts
@@ -0,0 +1,10 @@
+import { env } from "@/env";
+import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
+
+export const db = drizzle({
+  schema,
+  connection: {
+    connectionString: env.DATABASE_URL,
+  },
+});
